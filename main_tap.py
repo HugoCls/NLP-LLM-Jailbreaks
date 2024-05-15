20f90ceace1e5ea0242112b63fa2a1e28db13e91
Remi <rem.broe@orange.fr>
2024-05-15 20:10:03 +0300

    update tap main instructions

--- main_tap.py
+++ main_tap.py
@@ -7,11 +7,12 @@ import datetime
 from LanguageModels.GPT import response
 
 
-
-
-
-
-
-
-
-
+# Plan TAP 
+# Get the first prompt
+# Give it to 2 attacker llms along with the sentence associated
+# For each attacker do :
+# Separate the improvement part from the actual new prompt
+# Give the new prompt to the evaluator at the same time with the on topic sentence and get the result
+# Give it to the target and get the result
+# Then give the target answer to the evaluator to see if its jailbreak
+# If not, get the best evalutor answer (random if the same) and get the associated prompt to the top to create 2 new attacker prompts
