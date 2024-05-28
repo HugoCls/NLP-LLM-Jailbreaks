8371653afa6a8919052c73f45a63c99b670821ee
JamesMaistret <92112832+JamesMaistret@users.noreply.github.com>
2024-05-28 12:40:34 +0200

    gemini

---
+++ LanguageModels/Gemini.py
@@ -0,0 +1,25 @@
+#pip install -q -U google-generativeai (requirement)
+
+import google.generativeai as genai
+from dotenv import load_dotenv
+import os
+import datetime
+
+load_dotenv()
+
+GEMINI_API_KEY = os.getenv('gemini_api_key')
+
+def response(prompt, model="gemini-1.5-flash"):
+
+    genai.configure(api_key=GEMINI_API_KEY)
+
+    model = genai.GenerativeModel(model)
+
+    message = model.generate_content(prompt)
+
+    final_message = f"{datetime.datetime.now()}::{model}::{message}".replace('\n', ' ')
+
+    with open(f"results/Gemini.txt", "a", encoding="utf-8") as f:
+        f.write(f"{final_message}\n")
+    
+    return final_message
